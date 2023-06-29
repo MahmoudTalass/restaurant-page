76dd50cc4892580d333914dc1f92aa7d472130c8
Mahmoud <mahmoud.talass@gmail.com>
2023-06-29 10:22:15 -0400

    Import page header and call loadHeader function

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
-import loadPage from './home-page'
 import resetStyle from './css/reset.css'
 import style from './css/style.css'
 
+import loadPage from "./home-page";
+import loadHeader from "./page-header";
+
+
+loadHeader()
 loadPage()
