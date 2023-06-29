9d23fdc1471055a44fa4318992a04475554efb92
Mahmoud <mahmoud.talass@gmail.com>
2023-06-29 11:23:45 -0400

    Add section for restaurant hours

--- src/home-page.js
+++ src/home-page.js
@@ -7,20 +7,39 @@ function loadPage() {
    const restaurantDescCont = document.createElement("article");
    const restaurantDesc = document.createElement("p");
    const mainPage = document.createElement("main");
+   const hoursContainer = document.createElement("article");
+   const hoursHeading = document.createElement("h2");
+   const weekdayHours = document.createElement("p");
+   const weekendHours = document.createElement("p");
+
+   hoursHeading.textContent = "Hours";
+   weekdayHours.textContent = "Weekdays: 9am - 7pm";
+   weekendHours.textContent = "Weekends: 10am - 5pm";
 
    pageImg.src = image;
    pageImg.alt = "Soup and Bread image";
-   mainPage.appendChild(pageImg);
 
    restaurantDesc.textContent =
       "Welcome to The Bread Bowl, where warm bread and comforting soups meet in perfect harmony. Indulge in our freshly baked artisanal breads and savor our soul-soothing soup creations. Customize your perfect pairing and experience a delightful culinary journey that nourishes both body and soul. Join us for a cozy and satisfying dining experience at The Bread Bowl.";
-   restaurantDescCont.appendChild(restaurantDesc);
-   mainPage.appendChild(restaurantDescCont);
 
-   pageImg.classList.add("img")
-   restaurantDesc.classList.add("description")
-   restaurantDescCont.classList.add("description-container")
-   mainPage.classList.add("home-page-content")
+   restaurantDescCont.appendChild(pageImg);
+   restaurantDescCont.appendChild(restaurantDesc);
+   hoursContainer.appendChild(hoursHeading);
+   hoursContainer.appendChild(weekdayHours);
+   hoursContainer.appendChild(weekendHours);
+
+   mainPage.appendChild(restaurantDescCont);
+   mainPage.appendChild(hoursContainer);
+
+   pageImg.classList.add("soup-and-bread-img");
+   restaurantDesc.classList.add("description");
+   restaurantDescCont.classList.add("description-container");
+   hoursContainer.classList.add("hours-container");
+   hoursHeading.classList.add("hours-heading");
+   weekdayHours.classList.add("hours", "weekday-hours");
+   weekendHours.classList.add("hours", "weekend-hours");
+
+   mainPage.classList.add("home-page-content");
 
    content.appendChild(mainPage);
 }
