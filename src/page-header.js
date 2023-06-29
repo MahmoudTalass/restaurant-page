346b6ffef05aa15b2727ae157954d28c47983522
Mahmoud <mahmoud.talass@gmail.com>
2023-06-29 10:20:40 -0400

    Create module to load page header and nav

---
+++ src/page-header.js
@@ -0,0 +1,28 @@
+const content = document.querySelector("#content");
+
+function loadHeader() {
+   const header = document.createElement("header");
+   const restaurantName = document.createElement("h1");
+   const navContainer = document.createElement("nav");
+
+   restaurantName.textContent = "Bread and Soup";
+   header.appendChild(restaurantName);
+   header.appendChild(navContainer);
+
+   const homeBtn = document.createElement("button");
+   const menuBtn = document.createElement("button")
+   const contactBtn = document.createElement("button")
+
+   homeBtn.textContent = "Home"
+   menuBtn.textContent = "Menu"
+   contactBtn.textContent = "Contact"
+
+   navContainer.appendChild(homeBtn)
+   navContainer.appendChild(menuBtn)
+   navContainer.appendChild(contactBtn)
+
+
+   content.appendChild(header);
+}
+
+export default loadHeader;
