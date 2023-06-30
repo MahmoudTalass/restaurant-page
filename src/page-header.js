d61746d27e33135c365ed510c0cb6486c5ac9d0d
Mahmoud <mahmoud.talass@gmail.com>
2023-06-30 17:28:42 -0400

    Move btns declerations to top of module

--- src/page-header.js
+++ src/page-header.js
@@ -4,27 +4,25 @@ function loadHeader() {
    const header = document.createElement("header");
    const restaurantName = document.createElement("h1");
    const navContainer = document.createElement("nav");
+   const homeBtn = document.createElement("button");
+   const menuBtn = document.createElement("button");
+   const contactBtn = document.createElement("button");
 
-   header.classList.add('page-header')
-   restaurantName.classList.add("page-heading")
-   navContainer.classList.add("page-nav")
+   header.classList.add("page-header");
+   restaurantName.classList.add("page-heading");
+   navContainer.classList.add("page-nav");
 
-   restaurantName.textContent = "Bread and Soup";
+   restaurantName.textContent = "Bread & Soup";
+   homeBtn.textContent = "Home";
+   menuBtn.textContent = "Menu";
+   contactBtn.textContent = "Contact";
+
    header.appendChild(restaurantName);
    header.appendChild(navContainer);
 
-   const homeBtn = document.createElement("button");
-   const menuBtn = document.createElement("button")
-   const contactBtn = document.createElement("button")
-
-   homeBtn.textContent = "Home"
-   menuBtn.textContent = "Menu"
-   contactBtn.textContent = "Contact"
-
-   navContainer.appendChild(homeBtn)
-   navContainer.appendChild(menuBtn)
-   navContainer.appendChild(contactBtn)
-
+   navContainer.appendChild(homeBtn);
+   navContainer.appendChild(menuBtn);
+   navContainer.appendChild(contactBtn);
 
    content.appendChild(header);
 }
