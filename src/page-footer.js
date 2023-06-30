8eaa813f087f80f663e5204a46128563db1e6c42
Mahmoud <mahmoud.talass@gmail.com>
2023-06-30 17:53:20 -0400

    Make all address components into one address p el

--- src/page-footer.js
+++ src/page-footer.js
@@ -2,22 +2,20 @@ const content = document.querySelector("#content");
 
 function loadFooter() {
    const footer = document.createElement("footer");
-    const addressContainer = document.createElement('div')
-
-   const addressHalf1 = document.createElement("p");
-   const addressHalf2 = document.createElement('p')
+   const addressContainer = document.createElement("div");
+   const address = document.createElement("p")
 
    footer.classList.add("footer");
-   addressContainer.classList.add("copyright");
+   addressContainer.classList.add("address-container");
+   address.classList.add("address")
 
-   addressHalf1.textContent = "123 Fakestreet Ave"
-   addressHalf2.textContent = "Somecity, CA 12345"
+   address.textContent = "Address: 123 Fakestreet Ave Somecity, CA 12345";
 
-   addressContainer.appendChild(addressHalf1)
-   addressContainer.appendChild(addressHalf2)
-   footer.appendChild(addressContainer)
+   addressContainer.appendChild(address)
 
-   content.appendChild(footer)
+   footer.appendChild(addressContainer);
+
+   content.appendChild(footer);
 }
 
 export default loadFooter;
