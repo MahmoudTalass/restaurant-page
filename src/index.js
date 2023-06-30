a547d825eeac334f17e34423198ea2a1375903a1
Mahmoud <mahmoud.talass@gmail.com>
2023-06-30 17:54:33 -0400

    Import footer and call loadFooter at the bottom

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import style from './css/style.css'
 
 import loadPage from "./home-page";
 import loadHeader from "./page-header";
+import loadFooter from './page-footer';
 
 
 loadHeader()
 loadPage()
+loadFooter()
