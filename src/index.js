885f0fc4d7b03c974b215488b3bcd3ef97167866
Mahmoud <mahmoud.talass@gmail.com>
2023-07-02 14:34:46 -0400

    Add nav button functionality
    Load the header and footer before any of the other parts because the
    footer element needs to be on the page before the other elements because
    they depend on it to be inserted on the page in the right location.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,30 @@
 import resetStyle from './css/reset.css'
 import style from './css/style.css'
 
-import loadPage from "./home-page";
+import loadHome from "./home-page";
+import loadMenu from './menu-page';
 import loadHeader from "./page-header";
 import loadFooter from './page-footer';
 
+const content = document.querySelector("#content")
+
 
 loadHeader()
-loadPage()
-loadFooter()
+loadFooter();
+
+const homeBtn = document.querySelector("#home-btn")
+const menuBtn = document.querySelector("#menu-btn")
+const contactBtn = document.querySelector("#contact-btn")
+
+homeBtn.addEventListener("click", () => {
+    content.removeChild(content.childNodes[2])
+    loadHome()
+})
+menuBtn.addEventListener("click", () => {
+    content.removeChild(content.childNodes[2]);
+    loadMenu()
+})
+
+
+loadHome()
+
