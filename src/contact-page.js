4e09c936f6e29f8deef5aece42e6f6f18fd1c15d
Mahmoud <mahmoud.talass@gmail.com>
2023-07-03 12:01:48 -0400

    Create contacts page

---
+++ src/contact-page.js
@@ -0,0 +1,47 @@
+import { footer } from "./page-footer";
+
+const content = document.querySelector("#content");
+
+function loadContacts() {
+   const mainContent = document.createElement("main");
+   const contactsContainer = dcoument.createElement("article");
+   const contactsHeading = document.createElement("h2");
+   const emailContainer = document.createElement("div");
+   const emailHeading = document.createElement("span");
+   const email = document.createElement("p");
+   const phoneNumContainer = document.createElement("div");
+   const phoneNumHeading = document.createElement("span");
+   const phoneNumber = document.createElement("p");
+
+   contactsHeading.textContent = "Contact Us";
+   emailHeading.textContent = "Email: ";
+   email.textContent = "fakeemail@fakeemail.com";
+   phoneNumHeading.textContent = "Phone number: ";
+   phoneNumber.textContent = "012-345-6789";
+
+   contactsContainer.classList.add("contacts-container");
+   contactsHeading.classList.add("contacts-heading");
+   emailContainer.classList.add("email-container");
+   emailHeading.classList.add("email-heading");
+   email.classList.add("email");
+   phoneNumContainer.classList.add("phone-num-container");
+   phoneNumHeading.classList.add("phone-num-heading");
+   phoneNumber.classList.add("phone-number");
+
+   mainContent.classList.add("home-page-content", "main-content");
+
+   emailContainer.appendChild(emailHeading);
+   emailContainer.appendChild(email);
+   phoneNumContainer.appendChild(phoneNumHeading);
+   phoneNumContainer.appendChild(phoneNumber);
+
+   contactsContainer.appendChild(emailContainer);
+   contactsContainer.appendChild(phoneNumContainer);
+
+   mainContent.appendChild(contactsHeading);
+   mainContent.appendChild(contactsContainer);
+
+   content.insertBefore(mainContent, footer);
+}
+
+export default loadContacts;
